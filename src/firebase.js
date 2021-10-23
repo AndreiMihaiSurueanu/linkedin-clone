21e2ff291f8ae64d9bed04acadfcbcf3a21a870e
andrei <git1@email.com>
2021-10-23 20:40:28 +0200

    Added Firebase integration

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUYyRxPuIeW6S3OCRIwMUyB3l-YWai4xs",
+  authDomain: "linkedin-clone9.firebaseapp.com",
+  projectId: "linkedin-clone9",
+  storageBucket: "linkedin-clone9.appspot.com",
+  messagingSenderId: "167120131386",
+  appId: "1: 167120131386: web: 4a05428ba1c7692840ac03",
+};
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
