694350257720c10abf11203bb721d9f1a4bb91a7
andrei <git1@email.com>
2021-10-23 19:02:05 +0200

    Added Compose message elements

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,9 +1,29 @@
 import React from "react";
+// Icons
+import {
+  Create,
+} from "@material-ui/icons";
 // Styles
 import "./Feed.css";
 
 function Feed() {
-  return <div className="feed"></div>;
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <Create />
+          <form>
+            <input
+              type="text"
+            />
+            <button type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
