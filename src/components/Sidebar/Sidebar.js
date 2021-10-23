4a18932fbf4b1c83a30404c15bcb6adb2770a723
andrei <git1@email.com>
2021-10-23 18:41:50 +0200

    Added Sidebar section

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,47 @@
 import React from "react";
+// Components
+import { Avatar } from "@material-ui/core";
 // Styles
 import "./Sidebar.css";
 
 function Sidebar() {
-  return <div className="sidebar"></div>;
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img src="http://tny.im/pTq" alt="Sidebar profile cover" />
+        <Avatar className="sidebar__avatar" />
+        <h2>User</h2>
+        <h4>Email</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,448</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
