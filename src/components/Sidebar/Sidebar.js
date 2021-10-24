d2a4700ed6dca5c7349d181d8921f508ce8831bc
andrei <git1@email.com>
2021-10-24 18:55:22 +0200

    Display User Info from Redux store
    * Use the userSlice form the Redux Store to retrieve information about the User and display it in the Sidebar component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,15 @@
 import React from "react";
+// Redux
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
 // Components
 import { Avatar } from "@material-ui/core";
 // Styles
 import "./Sidebar.css";
 
 function Sidebar() {
+  const user = useSelector(selectUser);
+
   const recentItem = (topic) => (
     <div className="sidebar__recentItem">
       <span className="sidebar__hash">#</span>
@@ -16,9 +21,9 @@ function Sidebar() {
     <div className="sidebar">
       <div className="sidebar__top">
         <img src="http://tny.im/pTq" alt="Sidebar profile cover" />
-        <Avatar className="sidebar__avatar" />
-        <h2>User</h2>
-        <h4>Email</h4>
+        <Avatar className="sidebar__avatar" src={user.photoUrl}>{user.displayName[0]}</Avatar>
+        <h2>{user.displayName}</h2>
+        <h4>{user.email}</h4>
       </div>
 
       <div className="sidebar__stats">
