4eaa7240197bd7de2922b50945dc30ec2d896ec5
andrei <git1@email.com>
2021-10-24 08:54:42 +0200

    Added pieces of state for the form input fields

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 // Styles
 import "./Login.css";
 
 function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+
   const loginToApp = (e) => {
     e.preventDefault();
   };
@@ -16,18 +21,26 @@ function Login() {
       <form>
         <input
           type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           placeholder="Full name (required if registering)"
         />
         <input
           type="text"
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
           placeholder="Profile pic URL (optional)"
         />
         <input
           type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
         />
         <input
           type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
         />
         <button type="submit" onClick={loginToApp}>
